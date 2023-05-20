a6201faa1a5008572e2cc5acdee59de2d6329a53
walo450 <hirwawalid43@gmail.com>
2023-05-20 03:39:40 +0200

    my_work

---
+++ my_work/betty_check.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main- the main function.
+ *
+ * Return: 0 always 
+*/
+int main()
+{
+	int age = 20;
+
+	printf("my age %d", age);
+	return (0);
+}
---
+++ my_work/fork.c
@@ -0,0 +1,22 @@
+#include "group.h"
+
+void forkexe(char **arstr, char *envp[])
+{
+	pid_t wpid;
+	int status;
+
+	wpid = fork();
+	if (wpid < 0)
+	{
+		perror("Error (fork)");
+		return;
+	}
+	if (wpid == 0)
+	{
+		if (execve(arstr[0], arstr, envp) == -1)
+		    perror("./shell: No such file or directory\n")
+    }
+	else
+	   wait(&status);
+
+}
